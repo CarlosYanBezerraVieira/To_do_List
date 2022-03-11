c7485278adf0bc67004e46a52179c791cdcdaf6b
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2022-03-11 12:44:06 -0300

    create list select and notselect

--- lib/modules/Home/home_controller.dart
+++ lib/modules/Home/home_controller.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import 'package:to_do_list/database/connection.dart';
 import 'package:to_do_list/models/taks_model.dart';
 import '../../repor/database_repositoty/database_repository.dart';
 
@@ -10,6 +9,13 @@ class HomeController extends GetxController {
       : _dataRepository = dataRepository;
 
   final tasks = <TaskModel>[].obs;
+  final tasksSelect = <TaskModel>[].obs;
+  final tasksNotSelect = <TaskModel>[].obs;
+
+  List<TaskModel> listNotSelect = [];
+  List<TaskModel> listSelect = [];
+
+  var size = 0.0.obs;
 
   @override
   Future<void> onInit() async {
@@ -20,6 +26,7 @@ class HomeController extends GetxController {
   Future<void> getTasks() async {
     final result = await _dataRepository.getListTask();
     tasks.assignAll(result);
+    filtrar();
   }
 
   void deleteTask(TaskModel taskModel) {
@@ -41,5 +48,23 @@ class HomeController extends GetxController {
     } catch (e) {
       print("erro ao editar task");
     }
+  }
+
+  void filtrar() {
+    tasks.removeWhere((task) {
+      if (task.value == 0) {
+        listNotSelect.add(task);
+        tasksNotSelect.assignAll(listNotSelect);
+
+        return true;
+      } else {
+        listSelect.add(task);
+        tasksSelect.assignAll(listSelect);
+        size.value = tasksSelect.length * 0.115;
+        return false;
+      }
+    });
+    listNotSelect = [];
+    listSelect = [];
   }
 }
