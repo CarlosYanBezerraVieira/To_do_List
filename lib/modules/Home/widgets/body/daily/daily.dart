cc559bfa888a1819b412518138a214d31a0ad4ed
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2022-03-11 12:42:19 -0300

    generating item according to bank

--- lib/modules/Home/widgets/body/daily/daily.dart
+++ lib/modules/Home/widgets/body/daily/daily.dart
@@ -2,42 +2,120 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:to_do_list/modules/Home/widgets/body/daily/widget/item_daily.dart';
 
-class Daily extends StatelessWidget {
-  Daily({Key? key}) : super(key: key);
+import '../../../../../Shared/values/icons_svg.dart';
+import '../../../../../Shared/widget/item_form.dart';
+import '../../../../../models/taks_model.dart';
+import '../../../home_controller.dart';
 
+class Daily extends StatelessWidget {
+  Daily({Key? key, required this.controller}) : super(key: key);
+  final HomeController controller;
   @override
   Widget build(BuildContext context) {
-    List<String> titles = ["Wake Up!", "Daily workout"];
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Material(
-            color: Colors.transparent,
-            elevation: 10,
-            child: Container(
-              height: 172,
-              width: Get.width * 0.9,
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    colors: [
-                      Color(0xff7F00FF),
-                      Color(0xffE100FF),
-                    ],
-                  )),
-              child: ListView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: 2,
-                itemBuilder: (context, index) {
-                  return ItemDaily(title: titles[index]);
-                },
+    List<TaskModel> tasks = controller.tasksSelect;
+
+    return Obx(() => Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Material(
+                color: Colors.transparent,
+                elevation: 10,
+                child: Container(
+                  height: Get.height * controller.size.value,
+                  width: Get.width * 0.875,
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        colors: [
+                          Color(0xff7F00FF),
+                          Color(0xffE100FF),
+                        ],
+                      )),
+                  child: ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: tasks.length,
+                    itemBuilder: (context, index) {
+                      return ItemDaily(
+                        model: tasks[index],
+                        onPressed: () {
+                          showDialog(
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                backgroundColor: Colors.white,
+                                actions: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      IconButton(
+                                          onPressed: () {
+                                            controller.deleteTask(tasks[index]);
+                                          },
+                                          icon: Icon(Icons.delete)),
+                                      IconButton(
+                                          onPressed: () => controller
+                                              .updateTask(tasks[index]),
+                                          icon: Icon(Icons.create)),
+                                    ],
+                                  ),
+                                ],
+                                contentPadding: EdgeInsets.all(0),
+                                content: Container(
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Align(
+                                        alignment: Alignment.bottomRight,
+                                        child: IconButton(
+                                            onPressed: () => Get.back(),
+                                            icon: Icon(Icons.close)),
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 10),
+                                        child: Column(
+                                          children: [
+                                            ItemForm(
+                                              enabled: false,
+                                              icons: IconsSvg.iconTitle,
+                                              label: "Titulo",
+                                            ),
+                                            ItemForm(
+                                              enabled: false,
+                                              icons: IconsSvg.iconCalendar,
+                                              label: "Data",
+                                            ),
+                                            ItemForm(
+                                              enabled: false,
+                                              icons: IconsSvg.iconTime,
+                                              label: "Time",
+                                            ),
+                                            ItemForm(
+                                              enabled: false,
+                                              label: "Description",
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                            context: context,
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-      ],
-    );
+          ],
+        ));
   }
 }
--- lib/modules/Home/widgets/body/daily/widget/item_daily.dart
+++ lib/modules/Home/widgets/body/daily/widget/item_daily.dart
@@ -1,68 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../../../../../models/taks_model.dart';
+
 class ItemDaily extends StatelessWidget {
-  const ItemDaily({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const ItemDaily({Key? key, required this.model, required this.onPressed})
+      : super(key: key);
+  final Function()? onPressed;
+  final TaskModel model;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 20, bottom: 20, right: 10, left: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              Checkbox(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(3)),
-                  checkColor: const Color(0xff7F00FF),
-                  fillColor: MaterialStateProperty.all<Color>(
-                    Colors.white,
-                  ),
-                  value: true,
-                  onChanged: (valor) {}),
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(title,
-                        textAlign: TextAlign.start,
+      padding: const EdgeInsets.only(top: 15, bottom: 20, right: 10, left: 10),
+      child: InkWell(
+        onTap: onPressed,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Checkbox(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    checkColor: const Color(0xff7F00FF),
+                    fillColor: MaterialStateProperty.all<Color>(
+                      Colors.white,
+                    ),
+                    value: true,
+                    onChanged: (valor) {}),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(model.title ?? "",
+                          textAlign: TextAlign.start,
+                          style: GoogleFonts.nunito(
+                            textStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          )),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "06.00 AM",
                         style: GoogleFonts.nunito(
                           textStyle: TextStyle(
                               color: Colors.white,
-                              fontSize: 16,
+                              fontSize: 14,
                               fontWeight: FontWeight.bold),
-                        )),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "06.00 AM",
-                      style: GoogleFonts.nunito(
-                        textStyle: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.chevron_right_outlined),
-                color: Colors.white,
-                iconSize: 30,
-              ),
-            ],
-          )
-        ],
+              ],
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: onPressed,
+                  icon: const Icon(Icons.chevron_right_outlined),
+                  color: Colors.white,
+                  iconSize: 30,
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
