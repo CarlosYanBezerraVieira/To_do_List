ec13a48d31724cddc1341baae5dd239e0400699f
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2022-04-16 11:42:59 -0300

    getting data saved in the bank and starting form with them

--- lib/modules/editTask/edi_task_controller.dart
+++ lib/modules/editTask/edi_task_controller.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:to_do_list/Shared/values/date_formatt.dart';
 import 'package:to_do_list/models/taks_model.dart';
 import 'package:to_do_list/modules/Home/home_controller.dart';
 import 'package:to_do_list/repor/database_repositoty/database_repository.dart';
@@ -15,9 +17,13 @@ class EdiTaskController extends GetxController {
 
   final titleController = TextEditingController(text: '');
   final timeController = TextEditingController(text: '');
-  final datatimeController = TextEditingController(text: '');
-  final descriptionController = TextEditingController(text: '');
+  final newtimeController = TextEditingController(text: '');
 
+  final datatimeController = TextEditingController(text: '');
+  final newdatatimeController = TextEditingController(text: '');
+  final descriptionController = TextEditingController(text: '');
+  DateTime date = DateTime.now();
+  DateTime hour = DateTime.now();
   late TaskModel task;
   @override
   void onInit() {
@@ -28,12 +34,17 @@ class EdiTaskController extends GetxController {
       datatimeController.text =
           task.datatime != null ? task.datatime.toString() : "";
       descriptionController.text = task.description ?? "";
-
-      //passar dados pros controlles
+      convertHourToString();
+      convertFromDbToInicialvalue();
     } else {
       return Get.back();
     }
     super.onInit();
+  }
+
+  void convertHourToString() {
+    newtimeController.text =
+        DateFormatt().convertHourFromSeconds(int.parse(timeController.text));
   }
 
   void updateTask() {
@@ -43,8 +54,8 @@ class EdiTaskController extends GetxController {
         _dataRepository.update(
             TaskModel(
               title: titleController.text,
-              datatime: datatimeController.text,
-              time: timeController.text,
+              datatime: converteMonthToSeconds(),
+              time: converteHourToSeconds(),
               description: descriptionController.text,
               value: task.value,
             ),
@@ -67,5 +78,57 @@ class EdiTaskController extends GetxController {
     } catch (e) {
       Exception("erro na hora de muda o valor");
     }
+  }
+
+  TimeOfDay convertHourFromDb() {
+    final date =
+        DateTime.fromMillisecondsSinceEpoch(int.parse(timeController.text));
+    return TimeOfDay(hour: date.hour, minute: date.minute);
+  }
+
+  int converteHourToSeconds() {
+    final timeString = timeController.text;
+    if (timeString != "") {
+      List<String> timeSplit = timeString.split(':');
+      var clock = int.tryParse(timeSplit[0]);
+      var minute = int.tryParse(timeSplit[1]);
+      var dateWithoutTime = DateTime(date.year, date.month, date.day);
+      if (clock != null && minute != null) {
+        clock = clock * 60 * 60 * 1000;
+        minute = minute * 60 * 1000;
+        var clockTotalinSeconds =
+            (clock + minute + dateWithoutTime.millisecondsSinceEpoch);
+
+        return clockTotalinSeconds;
+      }
+    }
+
+    return hour.millisecondsSinceEpoch;
+  }
+
+  int converteMonthToSeconds() {
+    final dateString = datatimeController.text;
+
+    if (dateString != '') {
+      return DateTime.parse(dateString).millisecondsSinceEpoch;
+    }
+    ;
+
+    return date.millisecondsSinceEpoch;
+  }
+
+  void convertFromDbToInicialvalue() {
+    final dateSeconds = int.parse(datatimeController.text);
+    final dateTime = DateTime.fromMillisecondsSinceEpoch(dateSeconds);
+    final dateFormat = DateFormat(
+      DateFormat.YEAR_NUM_MONTH_DAY,
+    ).format(dateTime);
+    final listDates = dateFormat.split("/");
+    if (int.parse(listDates[0]) < 10) {
+      listDates[0] = "0" + listDates[0];
+    }
+    final dateReplace = "${listDates[2]}-${listDates[0]}-${listDates[1]}";
+
+    newdatatimeController.text = dateReplace;
   }
 }
